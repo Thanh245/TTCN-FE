53a1aa3d93e6b386385559d28781838a3e310673
Pham Manh Dung <67779456+pmdung2711@users.noreply.github.com>
2020-12-11 08:51:34 +0700

    Initial Admin Basic Page
    - Add react-admin library
    - Create Product components
    - Create AdminPage

---
+++ src/admin/components/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+import { Card, CardContent, CardHeader } from '@material-ui/core';
+
+export const Dashboard =  () => (
+    <Card>
+        <CardHeader title="Welcome to TokiDoki Shop - Admin Dashboard" />
+        <CardContent>Demo Version 1.0</CardContent>
+    </Card>
+);
